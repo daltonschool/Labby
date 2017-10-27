7e5711f2c9580c5a66531a10e6ac5f0a07f502ff
Alex Studer <astuder@notetoscreen.com>
2017-10-27 10:52:29 -0400

    move calendar columns into separate component

---
+++ imports/ui/CalendarColumn.jsx
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
+
+import CalendarEvent from './CalendarEvent.jsx';
+
+export default class CalendarColumn extends Component {
+    render() {
+        var that = this;
+        return <div className="calendarColumn">
+            {this.props.events.filter(function(event) {
+                return (event.startTime.getDay() === that.props.day);
+            }).map(function(event) {
+                return <CalendarEvent key={event._id._str} event={event} />;
+            })}
+        </div>;
+    }
+}
--- imports/ui/CalendarGrid.jsx
+++ imports/ui/CalendarGrid.jsx
@@ -2,36 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import classnames from 'classnames';
 
-import CalendarEvent from './CalendarEvent.jsx';
+import CalendarColumn from './CalendarColumn.jsx';
 
 export default class CalendarGrid extends Component {
     render() {
        return <div>
-           <div className="calendarGridColumn">{this.props.events.filter(function(event) {
-               return (event.startTime.getDay() === 1);
-           }).map(function(event) {
-               return <CalendarEvent key={event._id._str} event={event} />;
-           })}</div>
-           <div className="calendarGridColumn">{this.props.events.filter(function(event) {
-               return (event.startTime.getDay() === 2);
-           }).map(function(event) {
-               return <CalendarEvent key={event._id._str} event={event} />;
-           })}</div>
-           <div className="calendarGridColumn">{this.props.events.filter(function(event) {
-               return (event.startTime.getDay() === 3);
-           }).map(function(event) {
-               return <CalendarEvent key={event._id._str} event={event} />;
-           })}</div>
-           <div className="calendarGridColumn">{this.props.events.filter(function(event) {
-               return (event.startTime.getDay() === 4);
-           }).map(function(event) {
-               return <CalendarEvent key={event._id._str} event={event} />;
-           })}</div>
-           <div className="calendarGridColumn">{this.props.events.filter(function(event) {
-               return (event.startTime.getDay() === 5);
-           }).map(function(event) {
-               return <CalendarEvent key={event._id._str} event={event} />;
-           })}</div>
+           <CalendarColumn events={this.props.events} day={1} />
+           <CalendarColumn events={this.props.events} day={2} />
+           <CalendarColumn events={this.props.events} day={3} />
+           <CalendarColumn events={this.props.events} day={4} />
+           <CalendarColumn events={this.props.events} day={5} />
        </div>;
     }
 }
